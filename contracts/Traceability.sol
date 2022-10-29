6144c6e28113c302c69b714ced7e78fa5d06300d
Jonatan E. Duttweiler <jonatanduttweiler@gmail.com>
2022-10-29 15:36:54 -0300

    Update contracts to retrieve original sender

--- contracts/Traceability.sol
+++ contracts/Traceability.sol
@@ -1,27 +1,28 @@
-// SPDX-License-Identifier: GPL-3.0
+  // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+  pragma solidity >=0.7.0 <0.9.0;
+  import "./BaseRelayRecipient.sol";
 
-contract Traceability {
+  contract Traceability is BaseRelayRecipient {
 
-  address payable owner;
-  mapping(string => string) public hashes;
-  
-    constructor() { 
-      owner = payable(msg.sender); 
+    address payable owner;
+    mapping(string => string) public hashes;
+    
+      constructor() { 
+        owner = payable(_msgSender()); 
+      }
+
+      modifier onlyOwner {
+          require(
+              _msgSender() == owner,
+              "Only owner can call this function."
+          );
+          _;
+      }
+
+      function storeHash(string calldata traceId, string calldata hash) public onlyOwner {
+        require(bytes(hashes[traceId]).length == 0,"the key already exists");
+        hashes[traceId] = hash;
     }
 
-    modifier onlyOwner {
-        require(
-            msg.sender == owner,
-            "Only owner can call this function."
-        );
-        _;
-    }
-
-    function storeHash(string calldata traceId, string calldata hash) public onlyOwner {
-      require(bytes(hashes[traceId]).length == 0,"the key already exists");
-      hashes[traceId] = hash;
   }
-
-}

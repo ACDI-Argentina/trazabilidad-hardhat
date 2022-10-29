0207a326610e1a2450560064def59cccde19b6d5
Jonatan E. Duttweiler <jonatanduttweiler@gmail.com>
2022-10-29 15:45:25 -0300

    Add another transaction

--- scripts/deployTraceabilityLac.ts
+++ scripts/deployTraceabilityLac.ts
@@ -12,12 +12,14 @@ async function main() {
 
   // Add gas limit 3145728
   const storeHashTx = await traceability.storeHash("demo-id", "demo-hash", { gasLimit: 500000, gasPrice: 0 });
+  const storeHashTx2 = await traceability.storeHash("demo-id-2", "3f46dcaa0c39ec4bc2490a2eff06879e0b8b159fb5d28c521b99fca2a52b876c", { gasLimit: 500000, gasPrice: 0 });
 
   // wait until the transaction is mined
   await storeHashTx.wait();
+  await storeHashTx2.wait();
 
   
-  const stored = await traceability.hashes("demo-id");
+  const stored = await traceability.hashes("demo-id-2");
   console.log(stored);
 }
 
